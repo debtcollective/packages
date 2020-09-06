bd6da408b585c7c5f14083d29da3dadf3d51a5e1
Alexis Duran <alexis.duranm@gmail.com>
2020-09-06 09:20:36 +0200

    build(header-component): change namespace to be only "header"

--- packages/header-component/stencil.config.ts
+++ packages/header-component/stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import dotEnvPlugin from 'rollup-plugin-dotenv';
 
 export const config: Config = {
-  namespace: 'web-header',
+  namespace: 'header',
   taskQueue: 'async',
   outputTargets: [
     {
