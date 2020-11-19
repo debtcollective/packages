3ddae286127c2dd29b07a56fcb85724d400a40ef
Alexis Duran <alexis.duranm@gmail.com>
2020-11-19 18:39:55 +0100

    feat(collapser): make open class to live on parent node
    also this commit will format collapser file

--- packages/header-component/src/components/dc-header/dc-collapser.tsx
+++ packages/header-component/src/components/dc-header/dc-collapser.tsx
@@ -1,15 +1,10 @@
-import {
-  Component,
-  h,
-  State,
-  Prop,
-} from "@stencil/core";
-import omit from 'lodash.omit';
+import { Component, h, State, Prop, Host } from "@stencil/core";
+import omit from "lodash.omit";
 
 @Component({
   assetsDirs: ["assets"],
   tag: "dc-collapser",
-  styleUrl: "dc-collapser.css"
+  styleUrl: "dc-collapser.css",
 })
 export class Collapser {
   /**
@@ -21,7 +16,7 @@ export class Collapser {
    * Items to be displayed in the collapser
    */
   @Prop() items: string;
-  
+
   @State() open: boolean;
 
   toggleCollapser() {
@@ -30,32 +25,31 @@ export class Collapser {
 
   render() {
     //TODO: @Watch approach seems to not work correctly
-    const _items = JSON.parse(this.items)
+    const _items = JSON.parse(this.items);
 
     return (
-      <div class="nav-item">
-        <button
-          class="nav-link btn btn-transparent"
-          onClick={this.toggleCollapser.bind(this)}
-        >
-          {this.label || ''} 
-          <span class="material-icons">
-            {this.open ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
-          </span>
-        </button>
-        <ul class={`collapser-items ${this.open ? "open " : "hidden"}`}>
-          {
-            _items?.map(item => (
+      <Host class={`${this.open ? "open " : "collapsed"}`}>
+        <div class="nav-item">
+          <button
+            class="nav-link btn btn-transparent"
+            onClick={this.toggleCollapser.bind(this)}
+          >
+            {this.label || ""}
+            <span class="material-icons">
+              {this.open ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+            </span>
+          </button>
+          <ul class="collapser-items">
+            {_items?.map((item) => (
               <li class="collapser-item">
-                <a class="collapser-item-text" {...omit(item, ['text'])}>
+                <a class="collapser-item-text" {...omit(item, ["text"])}>
                   {item.text}
                 </a>
               </li>
-            ))
-          }
-        </ul>
-      </div>
+            ))}
+          </ul>
+        </div>
+      </Host>
     );
   }
 }
-  
