71ce167eb857402c048dc7a402048e6bc83921da
Alexis Duran <alexis.duranm@gmail.com>
2021-05-13 10:05:05 +0200

    feat(header): add base to render mobile menu nav

--- packages/header-component/src/components/dc-header/dc-menu.tsx
+++ packages/header-component/src/components/dc-header/dc-menu.tsx
@@ -31,8 +31,74 @@ export class Menu {
               close
             </button>
           </div>
-          <nav class="menu-section menu-nav">Menu nav</nav>
-          <div class="menu-section menu-footer">
+          <nav class="menu-section menu-nav">
+            <a href="/join-union" class="menu-nav-item menu-nav-text-lg">
+              Join the Union
+            </a>
+            <a href="/browse-events" class="menu-nav-item menu-nav-text-lg">
+              Browse Events
+            </a>
+            <details class="menu-nav-item-collapsable">
+              <summary class="menu-nav-text-lg">
+                Cancel Student Debt
+                <div class="material-icons icon icon-more">expand_more</div>{" "}
+                <div class="material-icons icon icon-less">expand_less</div>
+              </summary>
+              <div class="menu-nav-item-nested">
+                <a href="/sign" class="menu-nav-text">
+                  Sign the petition
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Email your reps
+                </a>
+              </div>
+            </details>
+            <details class="menu-nav-item-collapsable">
+              <summary class="menu-nav-text-lg">
+                For Members
+                <div class="material-icons icon icon-more">
+                  expand_more
+                </div>{" "}
+                <div class="material-icons icon icon-less">expand_less</div>
+              </summary>
+              <div class="menu-nav-item-nested">
+                <a href="/sign" class="menu-nav-text">
+                  Dispute your Debt
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Connect with other debtors
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Join a working group
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Join or start a local branch
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Join a Committee
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  View Past Trainings
+                </a>
+              </div>
+            </details>
+            <details class="menu-nav-item-collapsable">
+              <summary class="menu-nav-text-lg">
+                About{" "}
+                <div class="material-icons icon icon-more">expand_more</div>{" "}
+                <div class="material-icons icon icon-less">expand_less</div>
+              </summary>
+              <div class="menu-nav-item-nested">
+                <a href="/sign" class="menu-nav-text">
+                  Our Union
+                </a>
+                <a href="/sign" class="menu-nav-text">
+                  Learn about debt
+                </a>
+              </div>
+            </details>
+          </nav>
+          <div class="menu-section menu-footer mt-auto">
             <a class="icon" href="#">
               tw
             </a>
